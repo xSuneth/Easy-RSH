b00e7f9296c4943f5fc40d0766d8cde5260bd648
xusneth <sunethchathuranga2017@gmail.com>
2026-01-21 16:10:33 +0530

    fix: update Makefile - paths and server command mode

--- src/server/server_main.cpp
+++ src/server/server_main.cpp
@@ -55,6 +55,7 @@ int main(int argc, char* argv[]) {
         
         // Configure server
         server.setUseFork(use_fork);
+        server.setCommandMode(command_mode);
         
         // Start and run server
         server.start();
